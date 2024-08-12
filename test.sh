362d6f48e5f0b23c0b5f1d1da36680ab99b96ad1
Colin Hutchinson <hutchic@users.noreply.github.com>
2024-08-12 16:35:39 -0400

    feat(kong/build): setup the ability to build & test kong (#2)

--- test.sh
+++ test.sh
@@ -8,7 +8,24 @@ if [ -n "${DEBUG:-}" ]; then
 fi
 
 function test() {
-    ls -lah /tmp/build/out
+    echo '--- testing kong ---'
+    groupadd -g 10000 nobody || true
+
+    cp -R /tmp/build/* /
+    mv /tmp/build /tmp/buffer # Check we didn't link dependencies to `/tmp/build/...`
+
+    kong version
+    kong roar
+    #KONG_DATABASE=off kong start
+    #curl localhost:8001
+    #KONG_DATABASE=off kong restart
+    #curl localhost:8001
+    #kong stop
+
+    # Sleep long enough that Kong isn't running
+    #sleep 10
+    mv /tmp/buffer /tmp/build
+    echo '--- tested kong ---'
 }
 
 test
